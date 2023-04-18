3ff35983a23eab17b7ad9a4601b1fcd9321c241d
CharlVS <charl@vanstaden.info>
2023-04-18 11:20:42 +0200

    Compare isolate vs non-isolate

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:encrypt/encrypt.dart' as enc;
+import 'package:fl_chart/fl_chart.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,7 +29,8 @@ class BenchmarkBody extends StatefulWidget {
 }
 
 class _BenchmarkBodyState extends State<BenchmarkBody> {
-  final _results = <int, int>{};
+  final _resultsWithIsolate = <int, int>{};
+  final _resultsWithoutIsolate = <int, int>{};
   bool _isLoading = false;
 
   static Future<int> _deriveKey(int iterations) async {
@@ -48,9 +50,18 @@ class _BenchmarkBodyState extends State<BenchmarkBody> {
 
     for (int i = 0; i <= 100000; i += 10000) {
       final clampedI = i <= 0 ? 1 : i;
-      final elapsedMilliseconds = await compute(_deriveKey, clampedI);
+
+      // With isolate
+      final elapsedMillisecondsWithIsolate =
+          await compute(_deriveKey, clampedI);
       setState(() {
-        _results[clampedI] = elapsedMilliseconds;
+        _resultsWithIsolate[clampedI] = elapsedMillisecondsWithIsolate;
+      });
+
+      // Without isolate
+      final elapsedMillisecondsWithoutIsolate = await _deriveKey(clampedI);
+      setState(() {
+        _resultsWithoutIsolate[clampedI] = elapsedMillisecondsWithoutIsolate;
       });
     }
 
@@ -61,6 +72,29 @@ class _BenchmarkBodyState extends State<BenchmarkBody> {
 
   @override
   Widget build(BuildContext context) {
+    final lineBarsData = [
+      LineChartBarData(
+        spots: _resultsWithIsolate.entries
+            .map((entry) =>
+                FlSpot(entry.key.toDouble() / 10000, entry.value.toDouble()))
+            .toList(),
+        isCurved: true,
+        color: Colors.red,
+        barWidth: 2,
+        dotData: FlDotData(show: true),
+      ),
+      LineChartBarData(
+        spots: _resultsWithoutIsolate.entries
+            .map((entry) =>
+                FlSpot(entry.key.toDouble() / 10000, entry.value.toDouble()))
+            .toList(),
+        isCurved: true,
+        color: Colors.blue,
+        barWidth: 2,
+        dotData: FlDotData(show: true),
+      ),
+    ];
+
     return Stack(
       children: [
         ListView(
@@ -70,11 +104,84 @@ class _BenchmarkBodyState extends State<BenchmarkBody> {
               onPressed: _runBenchmark,
               child: Text('Run Benchmark'),
             ),
-            ..._results.entries.map(
-              (entry) => ListTile(
-                title: Text('${entry.key} iterations'),
-                trailing: Text('${entry.value} ms'),
+            SizedBox(height: 16),
+            AspectRatio(
+              aspectRatio: 1.5,
+              // height: 450,
+              child: LineChart(
+                LineChartData(
+                  lineBarsData: lineBarsData,
+                  minY: 0,
+                  maxY: (_resultsWithIsolate.isNotEmpty ||
+                          _resultsWithoutIsolate.isNotEmpty)
+                      ? (_resultsWithIsolate.values
+                              .followedBy(_resultsWithoutIsolate.values)
+                              .reduce((a, b) => a > b ? a : b) *
+                          1.1)
+                      : 100,
+                  titlesData: FlTitlesData(
+                      leftTitles: AxisTitles(
+                          axisNameWidget: Text('Compute time (seconds)'),
+                          sideTitles: SideTitles(
+                            showTitles: true,
+                            getTitlesWidget: (value, _) {
+                              final intValue = value.toInt();
+                              final readableLabel =
+                                  (intValue / 1000).toStringAsFixed(0) + 's';
+                              return Text(
+                                // intValue == 0 ? '' : intValue.toString(),
+                                readableLabel,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10,
+                                ),
+                              );
+                            },
+                          )),
+                      bottomTitles: AxisTitles(
+                        axisNameWidget: Text(
+                          'Iterations (x10k)',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 10,
+                          ),
+                        ),
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          // getTitles: (value) {
+                          //   final intValue = (value * 10000).toInt();
+                          //   return intValue == 0 ? '' : intValue.toString();
+                          // },
+                          getTitlesWidget: (value, _) {
+                            final intValue = (value * 10000).toInt();
+                            final readableLabel =
+                                (intValue / 1000).toStringAsFixed(0) + '';
+                            return Text(
+                              // intValue == 0 ? '' : intValue.toString(),
+                              readableLabel,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                              ),
+                            );
+                          },
+                        ),
+                      )),
+                  gridData: FlGridData(show: false),
+                  borderData: FlBorderData(show: false),
+                ),
               ),
+            ),
+            Text(
+              'Red: With Isolate',
+              style: TextStyle(color: Colors.red),
+            ),
+            Text(
+              'Blue: Without Isolate',
+              style: TextStyle(color: Colors.blue),
             ),
           ],
         ),
@@ -88,7 +195,7 @@ class _BenchmarkBodyState extends State<BenchmarkBody> {
                   CircularProgressIndicator(),
                   SizedBox(height: 16),
                   Text(
-                    'Processing ${_results.length * 10000} iterations...',
+                    'Processing ${_resultsWithIsolate.length * 10000} iterations...',
                     style: TextStyle(color: Colors.white),
                   ),
                 ],
